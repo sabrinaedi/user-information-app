43acb0004d4a499842ffa76d6de9d5740e78cb68
Sabedi <nsab@edinger.org>
2016-10-18 18:00:21 +0200

    installed body-parser and tried around a bit

--- app.js
+++ app.js
@@ -4,10 +4,14 @@
 const express = require('express')
 const fs = require('fs')
 const app = express()
+const bodyParser = require('body-parser')
+
 
 //adjust settings to view sttic pages in view folder
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
+
+app.use(bodyParser.json())
 
 // creates a route to render the index.pug file in the browser
 // adds readFile and Parse to go through Json-file
@@ -24,6 +28,10 @@ app.get('/index', (req, res) => {
 app.get('/search', (req, res) => {
 	console.log('search is running')
 	res.render('search')
+})
+
+app.post('/search', function(req, res) {
+	console.log(req.body)
 })
 
 // app listens to port 8000 for connetions

03dc854cc9ec9bed9571079fbccecee5306c3fe4
Sabedi <nsab@edinger.org>
2016-10-18 15:34:00 +0200

    staged .gitignore

--- app.js
+++ app.js
@@ -9,8 +9,19 @@ const app = express()
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
 
+//little test run
 app.get('/ping', (req, res) => {
 	res.send('Pong')
+})
+
+app.get('/index', (req, res) => {
+	console.log('I am running')
+	fs.readFile(__dirname + '/users.json', 'utf-8', (err, data) => {
+		if (err) throw err
+		let parsedData = JSON.parse(data)
+		console.log(parsedData)
+		res.render('index', {data: parsedData})
+	})
 })
 
 // app listens to port 8000 for connetions

c34ef7e311065e33c0c6fc2a202a2ab63785b960
Sabedi <nsab@edinger.org>
2016-10-18 16:44:59 +0200

    Part 1, route 2: added a route for page that displays a search bar

--- app.js
+++ app.js
@@ -9,22 +9,21 @@ const app = express()
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
 
-//little test run
-app.get('/ping', (req, res) => {
-	res.send('Pong')
-})
-
 // creates a route to render the index.pug file in the browser
 // adds readFile and Parse to go through Json-file
 // and render the results on the website via 'res.render'
 app.get('/index', (req, res) => {
-	console.log('I am running')
+	console.log('index is running')
 	fs.readFile(__dirname + '/users.json', 'utf-8', (err, data) => {
 		if (err) throw err
 		let parsedData = JSON.parse(data)
-		console.log(parsedData)
 		res.render('index', {data: parsedData})
 	})
+})
+
+app.get('/search', (req, res) => {
+	console.log('search is running')
+	res.render('search')
 })
 
 // app listens to port 8000 for connetions

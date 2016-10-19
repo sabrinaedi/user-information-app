e1246c333bfe620a64490bca1a25f9ac9e23ed6c
Sabedi <nsab@edinger.org>
2016-10-19 15:14:43 +0200

    completed route 3

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // creates a route to render the index.pug file in the browser
 // adds readFile and Parse to go through Json-file
@@ -30,9 +31,36 @@ app.get('/search', (req, res) => {
 	res.render('search')
 })
 
-app.post('/search', function(req, res) {
-	console.log(req.body)
-})
+
+app.post('/matches', function(req, res){ // Specifies which URL to listen for
+  // req.body -- contains form data
+  console.log(req.body)
+  var userReq = req.body.inputUser
+
+  let foundUser = []
+
+
+  fs.readFile(__dirname + '/users.json', 'utf-8', (err, data) => {
+		if (err) throw err
+		let parsedData = JSON.parse(data)
+		console.log(parsedData)
+		filterFunction (userReq, parsedData)
+
+
+/// write as a callback instead!!
+		function filterFunction (userReq, parsedData) {
+			for (var i=0; i<parsedData.length; i++) {
+			if ((parsedData[i].firstName || parsedData[i].lastName) == userReq) {
+				console.log(parsedData[i].firstName + " " + parsedData[i].lastName)
+				foundUser.push(parsedData[i].firstName, parsedData[i].lastName)
+				console.log(foundUser)
+				res.render('matches', {foundUser})
+				}
+			}
+		}
+	})
+});
+
 
 // app listens to port 8000 for connetions
 app.listen(8000, () => {
